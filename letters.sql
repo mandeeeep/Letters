ed4dfc87ca8250f4fa76323efeba150cede6bdaa
Mandeep Raj Shrestha <mandeep.raj.shrestha@gmail.com>
2014-03-17 13:39:19 +0545

    Added project SQL in master repo

---
+++ letters.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.12-log : Database - letters
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`letters` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `letters`;
+
+/*Table structure for table `letter` */
+
+DROP TABLE IF EXISTS `letter`;
+
+CREATE TABLE `letter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `to` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `from` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `subject` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `body` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `letter` */
+
+insert  into `letter`(`id`,`to`,`from`,`subject`,`body`) values (1,'Mandeep','Mane','Welcome to my world','Nothing is impossible!'),(7,'Insert TO1','Insert FROM1','Insert SUBJECT1','Insert BODY1');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
